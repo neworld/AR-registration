6fad9fe8111549b45e5d4099349ebe8a3c6e245c
neworld <aneworld@gmail.com>
2013-02-07 18:32:28 +0200

    Initalized Android project

---
+++ src/lt/neworld/arRegistration/MainActivity.java
@@ -0,0 +1,13 @@
+package lt.neworld.arRegistration;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+	}
+}
